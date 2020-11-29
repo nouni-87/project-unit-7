fb2ae9a957904fb43323f3035cc1079d631dc206
Hanane Ladj <hananeladj@gmail.com>
2020-11-29 16:31:23 -0700

    Styling the header

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const alertBanner = document.getElementById("alert");
 const dailyCanvas = document.getElementById("daily-chart");
+const mobileCanvas = document.getElementById("mobile-chart");
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
 // create the html for the banner
 alertBanner.innerHTML =
 `
@@ -72,4 +76,52 @@ const dailyData = {
     }
     }
     
+    let dailyChart = new Chart(dailyCanvas, {
+        type: 'bar',
+        data: dailyData,
+        options: dailyOptions
+        });
         
+        const mobileData = {
+            labels: ["Desktop", "Tablet", "Phones"],
+            datasets: [{
+            label: '# of Users',
+            data: [2000, 550, 500],
+            borderWidth: 0,
+            backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+            ]
+            }]
+            };
+
+            const mobileOptions = {
+                legend: {
+                position: 'right',
+                labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+                }
+                }
+                }
+
+                let mobileChart = new Chart(mobileCanvas, {
+                    type: 'doughnut',
+                    data: mobileData,
+                    options: mobileOptions
+                    });
+
+       
+                    send.addEventListener('click', () => {
+                        // ensure user and message fields are filled out
+                        if (user.value === "" && message.value === "") {
+                        alert("Please fill out user and message fields before sending");
+                        } else if (user.value === "" ) {
+                        alert("Please fill out user field before sending");
+                        } else if (message.value === "" ) {
+                        alert("Please fill out message field before sending");
+                        } else {
+                        alert(`Message successfully sent to: ${user.value}`);
+                        }
+                        });
